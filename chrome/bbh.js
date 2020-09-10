debab5ef7772130e7bbc5cc527cad3d25e428c59
Jeffrey Koopman <jeffrey.koopman@codam.nl>
2020-09-10 12:42:12 +0200

    Initial commit

---
+++ chrome/bbh.js
@@ -0,0 +1,3 @@
+var bhdate = document.getElementsByClassName("emote-bh")[0].getAttribute("data-original-title");
+document.getElementsByClassName("blackhole-title")[0].firstElementChild.innerText = bhdate;
+document.getElementById("bh").remove();
